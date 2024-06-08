2b7dd3ca9d864ad13d9fba85c2bb2662534e4fec
Letlaka Tsotetsi <Letlaka.t@gmail.com>
2024-06-09 01:40:29 +0200

    Update MenuModule.psm1

--- MenuModule.psm1
+++ MenuModule.psm1
@@ -1,3 +1,6 @@
+# MenuModule.psm1
+Import-Module -Name "$PSScriptRoot\ADModule.psm1"
+
 # Function to display an interactive menu
 function Show-Menu {
     param (
@@ -25,12 +28,19 @@ do {
     switch ($choice) {
         "1" {
             # Call function to check user information
+            $username = Read-Host -Prompt 'Enter the username to check'
+            Check-UserInformation -username $username
         }
         "2" {
             # Call function to reset user password
+            $username = Read-Host -Prompt 'Enter the username to reset password'
+            $newPassword = Read-Host -Prompt 'Enter the new password' -AsSecureString
+            Set-UserPassword -username $username -DCs $DCs -newPassword $newPassword
         }
         "3" {
             # Call function to unlock user account
+            $username = Read-Host -Prompt 'Enter the username to unlock account'
+            Unlock-UserAccount -username $username -DCs $DCs
         }
         "4" {
             Generate-AccountStatusSummary
