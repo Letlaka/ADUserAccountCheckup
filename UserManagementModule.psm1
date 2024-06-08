bcb9f0584347cc2c30c83d4f17811beb520773a7
Letlaka Tsotetsi <Letlaka.t@gmail.com>
2024-06-09 01:40:33 +0200

    Update UserManagementModule.psm1

--- UserManagementModule.psm1
+++ UserManagementModule.psm1
@@ -1,30 +1,53 @@
-# UserManagementModule.psm1
-
-function Set-UserPassword {
+function Check-UserInformation {
+    [CmdletBinding()]
     param (
-        [string]$username,
-        [array]$DCs,
-        [securestring]$newPassword
+        [Parameter(Mandatory=$true)]
+        [string]$username
     )
-    $resetSuccessful = $false
-    foreach ($DC in $DCs) {
-        try {
-            Set-ADAccountPassword -Identity $username -Server $DC -NewPassword $newPassword
-            Set-ADUser -Identity $username -Server $DC -ChangePasswordAtLogon $true
-            Write-Output "Password reset successfully on domain controller: $DC"
-            Write-LogAction "Password reset successfully on domain controller: $DC"
-            Send-EmailNotification -subject "Password Reset" -body "Password reset successfully on domain controller: $DC"
-            $resetSuccessful = $true
-            break  # Stop after successful reset on any DC
-        } catch {
-            Write-Warning "Error resetting password on domain controller: $DC"
-            Write-LogAction "Error resetting password on domain controller: $DC"
-        }
-    }
 
-    if (!$resetSuccessful) {
-        Write-Warning "Failed to reset the password on all domain controllers"
-        Write-LogAction "Failed to reset the password on all domain controllers"
-        Send-EmailNotification -subject "Password Reset Failed" -body "Failed to reset the password on all domain controllers"
+    $DCs = Get-ADDomainController -Discover -ErrorAction Stop
+    $credential = Get-Credential
+
+    $userInfo = Get-CachedUserInfo -username $username -servers $DCs -credential $credential
+    if ($userInfo) {
+        $DCInfo = @()
+
+        foreach ($DC in $DCs) {
+            $domainControllerInfo = [PSCustomObject]@{
+                DomainController      = $DC
+                Enabled               = $userInfo.Enabled
+                AccountExpirationDate = $userInfo.AccountExpirationDate
+                PasswordExpired       = $userInfo.PasswordExpired
+                PasswordAgeDays       = ((Get-Date) - $userInfo.PasswordLastSet).Days
+                PasswordNeverExpires  = $userInfo.PasswordNeverExpires
+                LockedOut             = $userInfo.LockedOut
+                LastLogonDate         = $userInfo.LastLogonDate
+            }
+
+            if ($userInfo.LockedOut) {
+                $domainControllerInfo
+            }
+
+            $DCInfo += $domainControllerInfo
+        }
+
+        Display-UserInfo -DCInfo $DCInfo -username $username
+
+        if ($userInfo.LockedOut) {
+            Write-Output "The account is locked out on the following domain controllers:"
+            $DCInfo | Where-Object { $_.LockedOut } | ForEach-Object { Write-Output $_ }
+
+            # Automatically unlock the account
+            Write-Output "Automatically unlocking the account on all domain controllers where it is locked out."
+            Unlock-UserAccount -username $username -DCs $DCs
+        } else {
+            Write-Output "The account is not locked out on any domain controller."
+        }
+    } else {
+        if ($DCs.Count -gt 0) {
+            Write-Warning "User not found on domain controller: $($DCs[0])"
+        } else {
+            Write-Warning "No domain controllers provided. Cannot check user information."
+        }
     }
 }
